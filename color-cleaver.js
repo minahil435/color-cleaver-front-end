8130e8d9aea0f11b1ea86ef4bb5b8a666910e6df
Minahil shafique <minahal.shafique@gmail.com>
2021-02-25 15:00:35 -0500

    output updated

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,36 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+
+
+const firstColor = getInput(1);
+const secondColor = getInput(2);
+
+if (firstColor === undefined) {
+    console.log("We need atleast one color");
+}
+else if (secondColor === undefined) {
+    const resultOfcolorDeconstructor = colorDeconstructor(firstColor);
+    if (resultOfcolorDeconstructor !== undefined) {
+        console.log("The color " + firstColor + " " + "is the combination of " + resultOfcolorDeconstructor);
+    }
+    else {
+        console.log("The color " + firstColor + " is not a primary color");
+    }
+}
+else {
+    const resultOfcolorCombinator = colorCombinator(firstColor, secondColor);
+    if (resultOfcolorCombinator !== undefined) {
+        const resultOfcolorCombinator = colorCombinator(firstColor, secondColor);
+        console.log("The colors," + firstColor + " and " + secondColor + " when combined result in  " + resultOfcolorCombinator);
+    }
+    else {
+        if (!(isValidSecondary(firstColor))) {
+            console.log("The color " + firstColor + " is not a secondary color");
+        }
+        else {
+            console.log("The color " + secondColor + " is not a secondary color");
+        }
+
+    }
+}
